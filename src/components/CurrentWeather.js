f1b3918dd5b42bb72fcfc997c5b6e1bc7a33a2b8
Therese Skaar Bjørknes <thereseskaar@hotmail.com>
2021-11-15 13:18:50 +0100

    basic component setup for CurrentWeather.js, fixed typo

--- src/components/CurrentWeather.js
+++ src/components/CurrentWeather.js
@@ -2,7 +2,7 @@ import React from "react";
 
 
 const CurrentWeather =(props)=>{
-  props = bergen;
+  props = "bergen";
   const apiCall = "https://timezone.abstractapi.com/v1/current_time/?api_key=dee439706fa54b019a4863ee627c2aa4&location=" + props;
   return apiCall;
 }
