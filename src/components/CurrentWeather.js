44a3fb0573dfe411a47d64ec79744d18a2e4ae47
Therese Skaar Bjørknes <thereseskaar@hotmail.com>
2021-11-15 13:17:42 +0100

    basic component setup for CurrentWeather.js

---
+++ src/components/CurrentWeather.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+
+const CurrentWeather =(props)=>{
+  props = bergen;
+  const apiCall = "https://timezone.abstractapi.com/v1/current_time/?api_key=dee439706fa54b019a4863ee627c2aa4&location=" + props;
+  return apiCall;
+}
+
+export default CurrentWeather;
+//apiKey = https://timezone.abstractapi.com/v1/current_time/?api_key=dee439706fa54b019a4863ee627c2aa4&location=Bergen, Norway
