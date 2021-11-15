6e271e8ad024ce359b19f6c6d07d911ce461ddbc
Therese Skaar Bjørknes <thereseskaar@hotmail.com>
2021-11-15 13:32:32 +0100

    update currentweather.js

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
+      const weather = <CurrentWeather city="Bergen"/>;
 
       </header>
     </div>
--- src/components/CurrentWeather.js
+++ src/components/CurrentWeather.js
@@ -2,8 +2,7 @@ import React from "react";
 
 
 const CurrentWeather =(props)=>{
-  props = "bergen";
-  const apiCall = "https://timezone.abstractapi.com/v1/current_time/?api_key=dee439706fa54b019a4863ee627c2aa4&location=" + props;
+  const apiCall = "https://timezone.abstractapi.com/v1/current_time/?api_key=dee439706fa54b019a4863ee627c2aa4&location=" + props.city;
   return apiCall;
 }
 
