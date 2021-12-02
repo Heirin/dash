1d136b31258f3bb033da72c35142e09c8431a906
Heirin <eirinbjorknes@gmail.com>
2021-12-02 13:49:16 +0100

    Hentet ut mer data og la til styling i CurrentWeather

--- src/App.js
+++ src/App.js
@@ -3,10 +3,16 @@ import CurrentWeather from './components/CurrentWeather';
 import CurrentTime from './components/CurrentTime';
 
 function App() {
+
+  const city = "Bergen";
+
   return (
     <div className="App">
       <header className="App-header">
-      <CurrentWeather city="Bergen"/>
+      <h1>{city}</h1>
+      <CurrentTime city={city}/>
+      <CurrentWeather city={city}/>
+      
     
 
       </header>
--- src/components/CurrentWeather.js
+++ src/components/CurrentWeather.js
@@ -1,12 +1,14 @@
 import React, {useState, useEffect} from "react";
-
+import "./Weather.css";
 
 const CurrentWeather =(props)=>{
   const [weather, setWeather] = useState("");
+  const [temperature, setTemperature] = useState("");
+  const [icon, setIcon] = useState("");
 
   useEffect(()=>{
     const apiKey = "32b1ef2fde08487f590d2266076d4f88";
-    const apiCall = "https://api.openweathermap.org/data/2.5/weather?q="+props.city+"&appid=" + apiKey;
+    const apiCall = "https://api.openweathermap.org/data/2.5/weather?q="+props.city+"&units=metric"+"&appid=" + apiKey;
     console.log(apiCall);
 
     const fetchData = async() => {
@@ -15,6 +17,8 @@ const CurrentWeather =(props)=>{
         const json = await response.json();
         console.log(json);
         setWeather(json.weather[0].description);
+        setTemperature(Math.round(json.main.temp*10)/10);
+        setIcon("https://openweathermap.org/img/wn/"+json.weather[0].icon+"@2x.png");
       }catch(error){
         console.log("error happened here");
         console.error(error);
@@ -25,7 +29,9 @@ const CurrentWeather =(props)=>{
 
   }, []);
 
-  return weather;
+  return <div className="weather">
+  <img src={icon} alt={weather} className="weather-icon"/>
+  <p> {temperature} C  </p> <p>{weather}</p></div>;
 
 }
 
